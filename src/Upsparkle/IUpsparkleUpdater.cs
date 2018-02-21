2a84a6590a0192652a751f1e9d4b09fc2ca498c1
Junian <git@junian.net>
2018-02-21 08:49:16 +0700

    add IUpsparkleUpdater interface

---
+++ src/Upsparkle/IUpsparkleUpdater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juniansoft.Upsparkle
+{
+    public delegate void Callback();
+
+    internal interface IUpsparkleUpdater
+    {
+        void Init();
+        void Cleanup();
+        void SetLang(string lang);
+        void SetLangId(ushort lang);
+        void SetAppcastUrl(string url);
+        void SetAppDetails(string companyName,
+            string appName,
+            string appVersion);
+        void SetAppBuildVersion(string build);
+        void SetRegistryPath(string path);
+        bool AutomaticCheckForUpdates { get; set; }
+        TimeSpan UpdateCheckInterval { get; set; }
+        DateTime LastCheckTime { get; }
+
+        void SetErrorCallback(Callback callback);
+        void SetCanShutdownCallback(Callback callback);
+        void SetShutdownRequestCallback(Callback callback);
+        void SetDidFindUpdateCallback(Callback callback);
+        void SetDidNotFindUpdateCallback(Callback callback);
+        void SetUpdateCancelledCallback(Callback callback);
+
+        void CheckUpdateWithUI();
+        void CheckUpdateWithUIAndInstall();
+        void CheckUpdateWithoutUI();
+    }
+}
