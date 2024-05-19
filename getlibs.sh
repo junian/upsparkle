560f0dfee64f27515edde2f6068bd3ffb989648a
Junian Triajianto <author@junian.dev>
2024-05-19 13:30:36 +0700

    Update WinSparkle

--- getlibs.sh
+++ getlibs.sh
@@ -14,8 +14,17 @@ echo "Extracting files ..."
 mkdir -p libs/Sparkle
 tar -jxvf libs/Sparkle*.tar.bz2 -C libs/Sparkle
 rm libs/Sparkle*.tar.bz2
+
+echo "Download and extract WinSparkle"
+
 unzip -o libs/WinSparkle*.zip -d libs
+
+mkdir -p libs/WinSparkle
+mv libs/WinSparkle-*/Release/WinSparkle.dll libs/WinSparkle/WinSparkle.x86.dll
+mv libs/WinSparkle-*/x64/Release/WinSparkle.dll libs/WinSparkle/WinSparkle.x86_64.dll
+mv libs/WinSparkle-*/ARM64/Release/WinSparkle.dll libs/WinSparkle/WinSparkle.arm64.dll
+
 rm libs/WinSparkle*.zip
-ls libs/ | grep WinSparkle | xargs -I '{}' mv libs/{} libs/WinSparkle
+rm -rf libs/WinSparkle-*
 
 echo "Files extraction finished. You can use them for development now."
